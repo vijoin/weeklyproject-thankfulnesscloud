92dd6803a15820b901547108b888b1e6c9761198
Victor Inojosa <victor.inojosa@atos.net>
2019-11-25 11:55:32 -0300

    Use Mongo Atlas as cloud database

--- app/application.py
+++ app/application.py
@@ -1,11 +1,16 @@
+import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_pymongo import PyMongo
 from forms.register import RegisterThankful
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 bootstrap = Bootstrap(app)
+
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -21,6 +26,7 @@ def register():
             'age': registerform.age.data,
             'description': registerform.description.data,
         }
+        mongo.db.thankfulness.insert_one(data)
         return render_template('thanks.html', data=data)
     return render_template('register.html', form=registerform)
 
